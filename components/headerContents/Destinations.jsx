9d2fede32524c7e9b896fee2514f56a10e35424f
DeepuGagan <gagandeepu717@gmail.com>
2025-04-03 23:56:03 +0530

    add components for header

---
+++ components/headerContents/Destinations.jsx
@@ -0,0 +1,226 @@
+import React from 'react'
+
+const Destinations = () => {
+  return (
+    <div
+      className="absolute top-full mt-4 rounded-3xl overflow-hidden bg-white rounded drop-shadow-sm max-w-5xl @7xl:max-w-none left-[21%] -translate-x-[21%] xl:-left-8 xl:translate-x-0"
+      data-headlessui-state="open"
+      id="headlessui-popover-panel-:r0:"
+      tabIndex="-1"
+    >
+      <div className="flex gap-x-6 w-content px-0 py-0 pr-6">
+        <div className="w-[230px] shrink-0 border-r border-black-200">
+          <button
+            className="block w-full text-left my-2 py-1 pl-4 @7xl:pl-6 pr-4 text-black first:mt-8 last:mb-8 hover:text-blue hover:bg-black-100 bg-blue-100 text-blue font-semibold"
+            data-title="Africa"
+            type="button"
+          >
+            Africa
+          </button>
+          <button
+            className="block w-full text-left my-2 py-1 pl-4 @7xl:pl-6 pr-4 text-black first:mt-8 last:mb-8 hover:text-blue hover:bg-black-100"
+            data-title="Antarctica"
+            type="button"
+          >
+            Antarctica
+          </button>
+          <button
+            className="block w-full text-left my-2 py-1 pl-4 @7xl:pl-6 pr-4 text-black first:mt-8 last:mb-8 hover:text-blue hover:bg-black-100"
+            data-title="Asia"
+            type="button"
+          >
+            Asia
+          </button>
+          <button
+            className="block w-full text-left my-2 py-1 pl-4 @7xl:pl-6 pr-4 text-black first:mt-8 last:mb-8 hover:text-blue hover:bg-black-100"
+            data-title="Australia & the Pacific"
+            type="button"
+          >
+            Australia & the Pacific
+          </button>
+          <button
+            className="block w-full text-left my-2 py-1 pl-4 @7xl:pl-6 pr-4 text-black first:mt-8 last:mb-8 hover:text-blue hover:bg-black-100"
+            data-title="The Caribbean"
+            type="button"
+          >
+            The Caribbean
+          </button>
+          <button
+            className="block w-full text-left my-2 py-1 pl-4 @7xl:pl-6 pr-4 text-black first:mt-8 last:mb-8 hover:text-blue hover:bg-black-100"
+            data-title="Central America"
+            type="button"
+          >
+            Central America
+          </button>
+          <button
+            className="block w-full text-left my-2 py-1 pl-4 @7xl:pl-6 pr-4 text-black first:mt-8 last:mb-8 hover:text-blue hover:bg-black-100"
+            data-title="Europe"
+            type="button"
+          >
+            Europe
+          </button>
+          <button
+            className="block w-full text-left my-2 py-1 pl-4 @7xl:pl-6 pr-4 text-black first:mt-8 last:mb-8 hover:text-blue hover:bg-black-100 cursor-on-hover"
+            data-title="Middle East"
+            type="button"
+          >
+            Middle East
+          </button>
+          <button
+            className="block w-full text-left my-2 py-1 pl-4 @7xl:pl-6 pr-4 text-black first:mt-8 last:mb-8 hover:text-blue hover:bg-black-100"
+            data-title="North America"
+            type="button"
+          >
+            North America
+          </button>
+          <button
+            className="block w-full text-left my-2 py-1 pl-4 @7xl:pl-6 pr-4 text-black first:mt-8 last:mb-8 hover:text-blue hover:bg-black-100"
+            data-title="South America"
+            type="button"
+          >
+            South America
+          </button>
+        </div>
+        <div className="py-8 shrink w-[450px] xxl:w-[600px]">
+          <div className="flex @5xl:flex-nowrap gap-x-4 @5xl:gap-x-6 gap-y-4 @5xl:gap-y-6">
+            <div className="w-1/2">
+              <p className="type-sm border-b border-black-200 font-semibold pb-1 mb-2">
+                Countries
+              </p>
+              <a
+                className="block py-1 hover:text-blue"
+                href="https://www.lonelyplanet.com/egypt"
+              >
+                Egypt
+              </a>
+              <a
+                className="block py-1 hover:text-blue"
+                href="https://www.lonelyplanet.com/ghana"
+              >
+                Ghana
+              </a>
+              <a
+                className="block py-1 hover:text-blue"
+                href="https://www.lonelyplanet.com/kenya"
+              >
+                Kenya
+              </a>
+              <a
+                className="block py-1 hover:text-blue cursor-on-hover"
+                href="https://www.lonelyplanet.com/madagascar"
+              >
+                Madagascar
+              </a>
+              <a
+                className="block py-1 hover:text-blue cursor-on-hover"
+                href="https://www.lonelyplanet.com/mauritius"
+              >
+                Mauritius
+              </a>
+              <a
+                className="block py-1 hover:text-blue"
+                href="https://www.lonelyplanet.com/morocco"
+              >
+                Morocco
+              </a>
+              <a
+                className="block py-1 hover:text-blue"
+                href="https://www.lonelyplanet.com/namibia"
+              >
+                Namibia
+              </a>
+              <a
+                className="block py-1 hover:text-blue"
+                href="https://www.lonelyplanet.com/south-africa"
+              >
+                South Africa
+              </a>
+              <a
+                className="block py-1 hover:text-blue"
+                href="https://www.lonelyplanet.com/tanzania"
+              >
+                Tanzania
+              </a>
+            </div>
+            <div className="w-1/3">
+              <p className="type-sm border-b border-black-200 font-semibold pb-1 mb-2">
+                Cities
+              </p>
+              <a
+                className="block py-1 hover:text-blue"
+                href="https://www.lonelyplanet.com/egypt/cairo"
+              >
+                Cairo
+              </a>
+              <a
+                className="block py-1 hover:text-blue"
+                href="https://www.lonelyplanet.com/south-africa/cape-town"
+              >
+                Cape Town
+              </a>
+              <a
+                className="block py-1 hover:text-blue"
+                href="https://www.lonelyplanet.com/morocco/marrakesh"
+              >
+                Marrakesh
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="shrink-0 ml-auto mr-0 w-60 py-8">
+          <article className="relative">
+            <img
+              alt="picture of Best in Travel"
+              className="max-w-full object-cover rounded-md aspect-[244/175]"
+              data-nimg="1"
+              decoding="async"
+              height="175"
+              loading="lazy"
+              src="https://lonelyplanetstatic.imgix.net/marketing/2024/global_nav/feature_destinations.png?auto=format&auto=format&fit=crop&q=75&w=640"
+              srcSet="https://lonelyplanetstatic.imgix.net/marketing/2024/global_nav/feature_destinations.png?auto=format&auto=format&fit=crop&q=75&w=384 1x, https://lonelyplanetstatic.imgix.net/marketing/2024/global_nav/feature_destinations.png?auto=format&auto=format&fit=crop&q=75&w=640 2x"
+              style={{
+                color: 'transparent'
+              }}
+              width="244"
+            />
+            <a
+              className="card-link mt-3 mb-2 flex gap-x-2.5 items-center hover:text-blue font-semibold"
+              href="https://www.lonelyplanet.com/best-in-travel"
+            >
+              Best in Travel
+            </a>
+            <p>
+              50 destinations for 50 years of travel
+            </p>
+          </article>
+        </div>
+      </div>
+      <div className="flex bg-black-100">
+        <a
+          className="flex w-[230px] shrink-0 border-r border-black-200 bg-black-100 items-center justify-between py-3 px-6 font-semibold text-black hover:bg-blue hover:text-white"
+          href="https://www.lonelyplanet.com/places"
+        >
+          See all destinations
+        </a>
+        <a
+          className="flex w-full bg-black-100 gap-x-2.5 items-center justify-end py-3 px-6 font-semibold text-black hover:bg-blue hover:text-white"
+          href="https://www.lonelyplanet.com/africa/places"
+        >
+          More in Africa
+          <svg
+            aria-hidden="true"
+            fill="currentColor"
+            height="16"
+            viewBox="0 0 16 16"
+            width="16"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M14.4998 7.80903C14.4742 7.74825 14.4372 7.69292 14.3908 7.64603L8.68084 1.93803C8.58696 1.84427 8.45967 1.79165 8.32699 1.79175C8.19431 1.79184 8.0671 1.84464 7.97334 1.93853C7.87959 2.03241 7.82697 2.1597 7.82707 2.29238C7.82716 2.42506 7.87996 2.55227 7.97384 2.64603L12.8278 7.50003H1.96484C1.83224 7.50003 1.70506 7.5527 1.61129 7.64647C1.51752 7.74024 1.46484 7.86742 1.46484 8.00003C1.46484 8.13263 1.51752 8.25981 1.61129 8.35358C1.70506 8.44735 1.83224 8.50003 1.96484 8.50003H12.8278L7.97384 13.354C7.87996 13.4478 7.82716 13.575 7.82707 13.7077C7.82697 13.8404 7.87959 13.9676 7.97334 14.0615C8.0671 14.1554 8.19431 14.2082 8.32699 14.2083C8.45967 14.2084 8.58696 14.1558 8.68084 14.062L14.3878 8.35403C14.4342 8.30713 14.4712 8.2518 14.4968 8.19103C14.5478 8.069 14.5489 7.93184 14.4998 7.80903Z" />
+          </svg>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default Destinations
---
+++ components/headerContents/Planner.jsx
@@ -0,0 +1,7 @@
+`organize and plan`
+
+
+`keep a btn`
+
+
+`this will be entry point to the thing i am working currently`
